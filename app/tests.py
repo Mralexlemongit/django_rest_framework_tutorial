4d6e93f8dc47d20272d38989f8b630e818839b58
alex arma <alex.cel.lemon@gmail.com>
2022-10-26 17:20:21 -0500

    Error corregido

--- app/tests.py
+++ app/tests.py
@@ -2,9 +2,6 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from app.views import snippet_detail, snippet_list, SnippetsList
-
-
 class SnippetsTestCase(TestCase):
     fixtures = ['snippets.json', ]
 
