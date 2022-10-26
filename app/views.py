f16872afc7a5197bc693247e6ed5a5810da51ef7
alex arma <alex.cel.lemon@gmail.com>
2022-10-26 17:18:39 -0500

    Reduciendo aun mas el codigo

--- app/views.py
+++ app/views.py
@@ -9,33 +9,13 @@ from rest_framework.response import Response
 from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetsList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class SnippetsList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
