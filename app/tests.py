0661cc191551d72ed4d01de9125a5aab0ada98ad
alex arma <alex.cel.lemon@gmail.com>
2022-10-26 16:42:05 -0500

    Funcional independientemente de la vista que lo genere, testeable

--- app/tests.py
+++ app/tests.py
@@ -1,58 +1,49 @@
 from django.test import TestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
 from rest_framework import status
 
-from app.views import snippet_detail, snippet_list
+from app.views import snippet_detail, snippet_list, SnippetsList
 
 
 class SnippetsTestCase(TestCase):
     fixtures = ['snippets.json', ]
 
     def setUp(self):
-        self.factory = APIRequestFactory()
+        self.client = APIClient()
 
     def test_get_snippets(self):
-        request = self.factory.get('/snippets/')
-        response = snippet_list(request)
+        response = self.client.get('/snippets/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_post_snippets(self):
         data = {"code": "Agregado en test_post_snippet"}
-        request = self.factory.post('/snippets/', data)
-        response = snippet_list(request)
+        response = self.client.post('/snippets/', data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_post_snippets_400_bad_request(self):
         data = {}
-        request = self.factory.post('/snippets/', data)
-        response = snippet_list(request)
+        response = self.client.post('/snippets/', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_snippet(self):
-        request = self.factory.get('/snippets/')
-        response = snippet_detail(request, 1)
+        response = self.client.get('/snippets/1')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_snippet_404_not_found(self):
-        request = self.factory.get('/snippets/')
-        response = snippet_detail(request, 4)
+        response = self.client.get('/snippets/4')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_post_snippet(self):
         data = {"code": "modificado en test_post_snippet"}
-        request = self.factory.get('/snippets/', data)
-        response = snippet_detail(request, 1)
+        response = self.client.post('/snippets/1', data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_post_snippet_400_bad_request(self):
         data = {"code": ""}
-        request = self.factory.post('/snippets/', data)
-        response = snippet_detail(request, 1)
+        response = self.client.post('/snippets/1', data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
     def test_delete_snippet(self):
-        data = {"code": ""}
-        request = self.factory.delete('/snippets/', data)
-        response = snippet_detail(request, 1)
+        response = self.client.delete('/snippets/1')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
     
--- app/views.py
+++ app/views.py
@@ -1,8 +1,48 @@
+from django.http import Http404
 from app.serializers import SnippetSerializer
 from app.models import Snippet
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+
+class SnippetsList(APIView):
+    def get(self, request):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SnippetDetail(APIView):
+    def get_snippet(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk):
+        snippet = self.get_snippet(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def post(self, request, pk):
+        snippet = self.get_snippet(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        snippet = self.get_snippet(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def snippet_list(request, format=None):
